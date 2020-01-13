4491dd8ab844a9c2d4607b01f56bb816f499e533
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-01-13 15:55:23 +0900

    Add ELF struct

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,79 @@
 mod header;
 mod program_header;
 
+use std::fmt;
 use std::fs;
 
-pub use header::*;
+pub use header::{Header, Class, get_elf_class};
+pub use program_header::ProgramHeader;
 
 fn main() {
     let binary = read_binary("hello.out");
-    match Header::<u32>::get_elf_class(&binary) {
+    match get_elf_class(&binary) {
         Some(Class::ELF64) => {
-            if let Some(header) = Header::<u64>::new(&binary) {
-                println!("{}", header);
+            match ELF::<u64>::new(&binary) {
+                Some(elf) => println!("{}", elf),
+                None => println!("{}", "Something Error!"),
             }
         }
         _ => (),
+    }
+}
+
+struct ELF<T>
+    where T: fmt::Display + fmt::Debug + fmt::LowerHex + Copy
+{
+    header: Header<T>,
+    program_headers: Vec<ProgramHeader<T>>
+}
+
+impl ELF<u32>
+{
+    pub fn new(binary: &Vec<u8>) -> Option<ELF<u32>> {
+        let header = Header::<u32>::new(binary)?;
+        let program_headers = ProgramHeader::<u32>::new(binary, &header)?;
+
+        Some(ELF {
+            header,
+            program_headers,
+        })
+    }
+}
+
+impl ELF<u64>
+{
+    pub fn new(binary: &Vec<u8>) -> Option<ELF<u64>> {
+        let header = Header::<u64>::new(binary)?;
+        let program_headers = ProgramHeader::<u64>::new(binary, &header)?;
+
+        Some(ELF {
+            header,
+            program_headers,
+        })
+    }
+}
+
+impl<T> fmt::Display for ELF<T>
+    where T: fmt::Display + fmt::Debug + fmt::LowerHex + Copy
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let program_header_string =
+            self.program_headers
+                .iter()
+                .zip(0..)
+                .map(|(ph, index)| {
+                    let ph_string = ph.to_string();
+                    let ph_string = ph_string
+                        .split("\n")
+                        .collect::<Vec<&str>>()
+                        .join("\n    ");
+
+                    format!("[{}]\n    {}", index, ph_string)
+                })
+                .collect::<Vec<String>>()
+                .join("\n");
+
+        write!(f, "{}\n\n{}", self.header, program_header_string)
     }
 }
 
